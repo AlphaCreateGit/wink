3ea624af3b6d44e5a2b1155bc6ea61b6f7f0658f
hai <hai.alphacreative@gmail.com>
2024-10-25 16:51:22 +0700

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ function gsapIntro() {
 
   // First animation: image-container img
   tl.to(".image-container img", {
-    scale: 5,
+    scale: 4,
     transformOrigin: "center center",
     ease: "power1.inOut",
     duration: 2, // Optional duration for the first animation
@@ -112,7 +112,7 @@ function gsapIntro() {
       transform: "skew(0deg)",
       transformOrigin: "center center",
       ease: "power2.inOut",
-      duration: 1.5, // Optional duration for the second animation
+      duration: 2, // Optional duration for the second animation
     },
     0.65
   ); // Start at the same time as the previous animation
